25bb7404c7067c66d81661d3c7faad3c0476f3ed
uu <emilylu369@gmail.com>
2019-03-18 13:02:44 -0400

    1:02pm

--- client/Main.js
+++ client/Main.js
@@ -1,19 +1,29 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Stories from './Stories'
+import SingleStory from './SingleStory'
+import {HashRouter, Route} from 'react-router-dom'
+
+
+
+
 
 export default class Main extends React.Component {
-  render () {
-    return (
-      <div id='main'>
-        <div className='column container'>
-          <div id='header'>
-            <h1>Readium</h1>
+  render () {  
+    return ( 
+      <HashRouter>
+        <div id='main'> 
+          <div className='column container'>
+            <div id='header'>
+              <h1>Readium</h1>
+            </div>
+            <Navbar /> 
           </div>
-          <Navbar />
+          <Route exact path='/stories' component = {Stories} />
+          <Route exact path='/' component = {Stories} />
+          <Route exact path='/stories/:storyId' component = {SingleStory} />
         </div>
-        <Stories />
-      </div>
+      </HashRouter>
     )
   }
 }
